f62fde564d9efe294a3537b92fb9561e87d13125
Zehui ZH2 Mao <maozh2@Lenovo.com>
2026-07-14 10:49:52 +0800

    feat: use integrated HTML prototype as frontend

--- diaoleme/src/App.tsx
+++ diaoleme/src/App.tsx
@@ -1,28 +1,27 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import TabLayout from './layouts/TabLayout'
-import Hello from './pages/Hello'
-import Scan from './pages/Scan'
-import Report from './pages/Report'
-import Tasks from './pages/Tasks'
-import Hairstyle from './pages/Hairstyle'
-import Records from './pages/Records'
-import Me from './pages/Me'
+import { useEffect, useRef } from 'react'
+import { prototypeBody, prototypeScript, prototypeStyle } from './prototypeHtml'
 
 export default function App() {
-  return (
-    <div className="relative h-[100dvh] w-[448px] max-w-full bg-cream shadow-2xl shadow-coffee/20 overflow-hidden">
-      <Routes>
-        <Route path="/" element={<Hello />} />
-        <Route element={<TabLayout />}>
-          <Route path="/tab/scan" element={<Scan />} />
-          <Route path="/tab/report" element={<Report />} />
-          <Route path="/tab/records" element={<Records />} />
-          <Route path="/tab/tasks" element={<Tasks />} />
-          <Route path="/tab/hairstyle" element={<Hairstyle />} />
-          <Route path="/tab/me" element={<Me />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </div>
-  )
+  const rootRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    let styleTag = document.getElementById('diaoleme-prototype-style') as HTMLStyleElement | null
+    if (!styleTag) {
+      styleTag = document.createElement('style')
+      styleTag.id = 'diaoleme-prototype-style'
+      document.head.appendChild(styleTag)
+    }
+    styleTag.textContent = prototypeStyle
+
+    if (rootRef.current) {
+      rootRef.current.innerHTML = prototypeBody
+      new Function(prototypeScript)()
+    }
+
+    return () => {
+      if (rootRef.current) rootRef.current.innerHTML = ''
+    }
+  }, [])
+
+  return <div ref={rootRef} />
 }
